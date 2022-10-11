9d338472dfe6ae0e19c32c888094979a67e0c92e
abhilashadwivedi <abhilashadwivedi74@gmail.com>
2022-10-11 11:46:15 +0530

    saving info and showing on the screen

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,14 +1,21 @@
 import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import CreateTask from '../modals/CreateTask';
+import CreateTaskPopUp from '../modals/CreateTask';
 
 const TodoList = () => {
     const [modal, setModal] = useState(false);
+    const [taskList , setTaskList] = useState([])
 
     const toggle = () => {
         setModal(!modal);
     }
 
+    const saveTask = (taskObj) => {
+        let tempList = taskList
+        tempList.push(taskObj)
+        setTaskList(tempList)
+        setModal(false)
+    }
     return (
         <>
         <div className = "header text-center">
@@ -16,9 +23,9 @@ const TodoList = () => {
             <button className="btn btn-primary mt-2" onClick ={() => setModal(true)}>Create Task</button>
         </div>
         <div className = "task-container">
-
+            {taskList.map((obj) => <li>{obj.Name}</li>)}
         </div>
-        <CreateTask toggle = {toggle} modal={modal}></CreateTask>
+        <CreateTaskPopUp toggle = {toggle} modal={modal} save = {saveTask}></CreateTaskPopUp>
         </>
     );
 };
